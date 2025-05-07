1d75a4888d249082128a0a3d52406bafe05a2812
manuel alfaro <manuel.alfaro247@gmail.com>
2025-05-07 15:12:00 -0500

    Login Tracker

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
 
+  return (inputPassword) => {
+    if (isLocked) {
+      return 'Account locked due to too many failed login attempts';
+    }
 
+    if (inputPassword === userInfo.password) {
+      return 'Login successful';
+    }
+
+    attemptCount++;
+
+    // Return failed attempt message first
+    const failMessage = `Attempt ${attemptCount}: Login failed`;
+
+    // Lock the account only *after* 3rd attempt
+    if (attemptCount >= 3) {
+      isLocked = true;
+    }
+
+    return failMessage;
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
